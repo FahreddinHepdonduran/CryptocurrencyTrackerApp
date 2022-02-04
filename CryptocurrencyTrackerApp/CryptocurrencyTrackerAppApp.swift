77426f7b850e530512468b313a7a8bd980fbcc54
FahreddinHepdonduran <fahreddinhepdonduran@gmail.com>
2022-02-04 15:50:10 +0300

    Initial Commit

---
+++ CryptocurrencyTrackerApp/CryptocurrencyTrackerAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptocurrencyTrackerAppApp.swift
+//  CryptocurrencyTrackerApp
+//
+//  Created by fahreddin on 4.02.2022.
+//
+
+import SwiftUI
+
+@main
+struct CryptocurrencyTrackerAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
