5de8acee8f1caa774b5d49d4fe34df5bf6872b38
FahreddinHepdonduran <fahreddinhepdonduran@gmail.com>
2022-02-05 18:21:54 +0300

    add Top List Row View file

---
+++ CryptocurrencyTrackerApp/Views/TopListRowView/TopListRowView.swift
@@ -0,0 +1,20 @@
+//
+//  TopListRowView.swift
+//  CryptocurrencyTrackerApp
+//
+//  Created by fahreddin on 5.02.2022.
+//
+
+import SwiftUI
+
+struct TopListRowView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct TopListRowView_Previews: PreviewProvider {
+    static var previews: some View {
+        TopListRowView()
+    }
+}
