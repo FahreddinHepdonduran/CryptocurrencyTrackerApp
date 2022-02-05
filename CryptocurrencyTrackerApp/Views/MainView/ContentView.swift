7d1ab7dfdd141ea2ad178f9d0ccfdbc3bf91d413
FahreddinHepdonduran <fahreddinhepdonduran@gmail.com>
2022-02-05 20:11:46 +0300

    add dummy top list rows

--- CryptocurrencyTrackerApp/Views/MainView/ContentView.swift
+++ CryptocurrencyTrackerApp/Views/MainView/ContentView.swift
@@ -9,15 +9,27 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        LinearGradient(
-            colors:
-                [
-                    Color(#colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)),
-                    Color(#colorLiteral(red: 0.4588235294, green: 0.2745098039, blue: 0.5058823529, alpha: 1))
-                ],
-            startPoint: .top,
-            endPoint: .bottom
-        ).edgesIgnoringSafeArea(.all)
+        ZStack {
+            LinearGradient(
+                colors:
+                    [
+                        Color(#colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)),
+                        Color(#colorLiteral(red: 0.4588235294, green: 0.2745098039, blue: 0.5058823529, alpha: 1))
+                    ],
+                startPoint: .top,
+                endPoint: .bottom
+            ).edgesIgnoringSafeArea(.all)
+            
+            ScrollView(.horizontal) {
+                HStack(spacing: 14.0) {
+                    ForEach(0..<10) { _ in
+                        TopListRowView()
+                            .cornerRadius(21.0)
+                        
+                    }
+                }
+            }.frame(height: 210.0)
+        }
     }
 }
 
