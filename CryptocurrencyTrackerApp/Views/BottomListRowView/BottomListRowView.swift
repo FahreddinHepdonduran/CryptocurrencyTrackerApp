f8e03573f1b34c21af9dd08697a7bf9154a24471
FahreddinHepdonduran <fahreddinhepdonduran@gmail.com>
2022-02-06 18:28:35 +0300

    complete BottomListRowView

--- CryptocurrencyTrackerApp/Views/BottomListRowView/BottomListRowView.swift
+++ CryptocurrencyTrackerApp/Views/BottomListRowView/BottomListRowView.swift
@@ -12,7 +12,25 @@ struct BottomListRowView: View {
         ZStack {
             Color("BottomListRowBackgroundColor")
             
-            
+            HStack {
+                Image(systemName: "person.crop.circle")
+                    .resizable()
+                    .frame(width: 46.0, height: 46.0)
+                    .padding(.leading, 16.0)
+                
+                Spacer(minLength: 22.0)
+                
+                Text("BTC")
+                    .font(.custom("Roboto", size: 16.0))
+                    .foregroundColor(.white)
+                
+                Spacer(minLength: 71.0)
+                
+                Text("$46464646")
+                    .font(.custom("Roboto", size: 16.0))
+                    .foregroundColor(.white)
+                    .padding(.trailing, 43.0)
+            }.frame(width: 317.0, height: 77.0)
         }
     }
 }
