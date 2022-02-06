fabc11f42bb6079d48d491246c4be2d71ba37685
FahreddinHepdonduran <fahreddinhepdonduran@gmail.com>
2022-02-06 14:37:25 +0300

    add BottomListRowView.swift file

---
+++ CryptocurrencyTrackerApp/Views/BottomListRowView/BottomListRowView.swift
@@ -0,0 +1,20 @@
+//
+//  BottomListRowView.swift
+//  CryptocurrencyTrackerApp
+//
+//  Created by fahreddin on 6.02.2022.
+//
+
+import SwiftUI
+
+struct BottomListRowView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct BottomListRowView_Previews: PreviewProvider {
+    static var previews: some View {
+        BottomListRowView()
+    }
+}
