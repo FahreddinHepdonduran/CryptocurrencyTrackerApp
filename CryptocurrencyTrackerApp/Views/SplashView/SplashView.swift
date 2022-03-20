e284bd619a17ddd72e2d7b639a0fe5f5155ec94e
FahreddinHepdonduran <fahreddinhepdonduran@gmail.com>
2022-03-20 13:28:50 +0300

    add splash view

---
+++ CryptocurrencyTrackerApp/Views/SplashView/SplashView.swift
@@ -0,0 +1,22 @@
+//
+//  SplashView.swift
+//  CryptocurrencyTrackerApp
+//
+//  Created by fahreddin on 20.03.2022.
+//
+
+import SwiftUI
+
+struct SplashView: View {
+    var body: some View {
+        Image("SplashIcon")
+            .resizable()
+            .frame(width: 192.0, height: 218.0)
+    }
+}
+
+struct SplashView_Previews: PreviewProvider {
+    static var previews: some View {
+        SplashView()
+    }
+}
