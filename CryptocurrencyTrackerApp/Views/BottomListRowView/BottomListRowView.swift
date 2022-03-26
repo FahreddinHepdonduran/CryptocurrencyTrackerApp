c07bec6d7467fb833ee92dfdb70c8066f94cb232
FahreddinHepdonduran <fahreddinhepdonduran@gmail.com>
2022-03-26 19:37:16 +0300

    remove background color

--- CryptocurrencyTrackerApp/Views/BottomListRowView/BottomListRowView.swift
+++ CryptocurrencyTrackerApp/Views/BottomListRowView/BottomListRowView.swift
@@ -12,29 +12,31 @@ struct BottomListRowView: View {
     let model: CoinAssetModel
     
     var body: some View {
-        ZStack {
-            Color("BottomListRowBackgroundColor")
+        HStack {
+            KFImage(model.iconUrlStringToUrl)
+                .resizable()
+                .frame(width: 46.0, height: 46.0)
+                .padding(.leading, 16.0)
             
-            HStack {
-                KFImage(URL(string: model.iconUrl)!)
-                    .resizable()
-                    .frame(width: 46.0, height: 46.0)
-                    .padding(.leading, 16.0)
-                
-                Spacer(minLength: 22.0)
-                
-                Text(model.symbol)
-                    .font(.custom("Roboto", size: 16.0))
-                    .foregroundColor(.white)
-                
-                Spacer(minLength: 71.0)
-                
-                Text(model.price)
-                    .font(.custom("Roboto", size: 16.0))
-                    .foregroundColor(.white)
-                    .padding(.trailing, 43.0)
-            }.frame(width: 317.0, height: 77.0)
-        }
+            Spacer(minLength: 22.0)
+            
+            Text(model.symbol)
+                .font(.custom("Roboto", size: 16.0))
+                .fontWeight(.regular)
+                .tracking(0.15)
+                .foregroundColor(.white)
+            
+            Spacer(minLength: 71.0)
+            
+            Text("\(model.roundedPrice) $")
+                .font(.custom("Roboto", size: 16.0))
+                .fontWeight(.semibold)
+                .tracking(0.15)
+                .foregroundColor(.white)
+                .minimumScaleFactor(0.5)
+                .lineLimit(1)
+                .multilineTextAlignment(.trailing)
+        }.frame(width: 317.0, height: 77.0)
     }
 }
 
